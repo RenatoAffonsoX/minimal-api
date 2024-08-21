360c79ed7780c2b709cb45eeed014ee07ed6b991
renatosaffonso <renatosaffonso@concilig.com.br>
2024-08-21 19:48:59 -0300

    Ajustando configurações

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Dominio.Entidades;
+using System;
+using Microsoft.AspNetCore.Components; 
+
 namespace MinimalAPI.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        configuracaoAppSettings = _configuracaoAppSettings;
+    }
+
+    public DbSet<Administradores> Administrador {get; set;} = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var stringConexao = _configuracaoAppSettings.GetConnectionString("SqlServer")?.ToString();
+
+        if (!optionsBuilder.IsConfigured || !string.IsNullOrEmpty(stringConexao))
+            optionsBuilder.UseSqlServer(stringConexao);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MinimalAPI.DTOs;
+using MinimalAPI.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(Options => 
+{
+    Options.UseSqlServer(
+        builder.Configuration.GetConnectionString("SqlServer")        
+    );
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
